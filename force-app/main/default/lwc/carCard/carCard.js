e1c6d3fdb9bf2838287de2ce15ac43a8f79478b6
leosantos <salles.lsantos@gmail.com>
2025-05-16 19:04:48 -0300

    car card

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -1,28 +1,46 @@
-import { api, track, LightningElement } from 'lwc';
+import { api, track, LightningElement, wire } from 'lwc';
+import {getRecord} from 'lightning/uiRecordApi';
+import CATEGORY from '@salesforce/schema/Car__c.Category__c';
+import MAKE from '@salesforce/schema/Car__c.Make__c';
+import MSRP from '@salesforce/schema/Car__c.MSRP__c';
+import FUEL from '@salesforce/schema/Car__c.Fuel_type__c';
+import SEATS from '@salesforce/schema/Car__c.Number_of_seats__c';
+import CONTROL from '@salesforce/schema/Car__c.Control__c';
+import PICTURE from '@salesforce/schema/Car__c.Picture_URL__c';
+import NAME from '@salesforce/schema/Car__c.Name';
+
+
 
 export default class carCard extends LightningElement {
 
-    @api recordId;
-    @api objectApiName;
+    recordId = 'a1DKe000000oQlGMAU';
+    name;
+    category;
+    make;
+    msrp;
+    fuel;
+    pictureUrl;
+    control;
+    seats;
 
-    // This is an example of a variable tracked by LWC
-    // the layout is rerendered if any tracked variable is changed
-    @track variable = null;
-
-    /**
-     * Executed when the component is added to the DOM.
-     * @returns {Promise<void> | void} 
-     */
-    async connectedCallback() {
-        // Todo: remove me or add code
+    @wire(getRecord,{
+        recordId: '$recordId',
+        fields:[CATEGORY,MAKE,MSRP,FUEL,SEATS,CONTROL,PICTURE,NAME]
+    })
+    carCardRecord({data,error}){
+        if(data){
+            console.log(data);
+            this.name = data.fields.Name.value;
+            this.category = data.fields.Category__c.value;
+            this.make = data.fields.Make__c.value;
+            this.msrp = data.fields.MSRP__c.value;
+            this.fuel = data.fields.Fuel_type__c.value;
+            this.control = data.fields.Control__c.value;
+            this.pictureUrl = data.fields.Picture_URL__c.value;
+            this.seats = data.fields.Number_of_seats__c.value;
+        }
+        }
+        if(error){
+            console.log('deu ruim: ',error);
+        }
     }
-
-    /**
-     * Executed by the framework when the LWC component is removed 
-     * from the DOM; use this to put any clean-up code when required
-     * @returns {Promise<void> | void} 
-     */
-    disconnectedCallback() {
-        // Todo: remove me or add code
-    }
-}
