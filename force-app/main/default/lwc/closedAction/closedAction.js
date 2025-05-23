6768c7e644182a28d8834cbb61e8480c5c40d58d
leosantos <salles.lsantos@gmail.com>
2025-05-23 19:10:42 -0300

    pequenos ajustes

--- force-app/main/default/lwc/closedAction/closedAction.js
+++ force-app/main/default/lwc/closedAction/closedAction.js
@@ -1,7 +1,7 @@
 import { api, track, LightningElement } from 'lwc';
 import { updateRecord } from 'lightning/uiRecordApi';
-import STAGE from '@salesforce/schema/Opportunity.StageName';
-import ID_FIELD from '@salesforce/schema/Opportunity.Id';
+//import STAGE from '@salesforce/schema/Opportunity.StageName';
+//import ID_FIELD from '@salesforce/schema/Opportunity.Id';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class closedAction extends LightningElement {
@@ -9,8 +9,9 @@ export default class closedAction extends LightningElement {
     @api recordId;
     
     @api invoke(){
-        
+        //invoke é chamado quando clicamos no botão da quick action
     
+        //montagem do objeto que será chamado no @wire
         const fields = {
         Id: this.recordId,
         StageName: 'Closed'
