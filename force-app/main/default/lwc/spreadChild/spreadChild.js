4afdb92d7d7fabab7be52d14e1cc4af8c408237f
leosantos <salles.lsantos@gmail.com>
2025-06-09 13:30:44 -0300

    lwc:spread

---
+++ force-app/main/default/lwc/spreadChild/spreadChild.js
@@ -0,0 +1,9 @@
+import { api, track, LightningElement } from 'lwc';
+
+export default class spreadChild extends LightningElement {
+    @api playerposition;
+    @api playername;
+    @api playerPropTwo;
+    @api nome;
+    @api posicao;
+}
---
+++ force-app/main/default/lwc/spreadParent/spreadParent.js
@@ -0,0 +1,15 @@
+import { api, track, LightningElement } from 'lwc';
+
+export default class spreadParent extends LightningElement {
+    playerProp = {
+        name: 'Luka Doncic',
+        position: 'PG',
+        number: '77'
+    }
+
+    playerPropTwo = {
+        nome: 'Nikola Jokic',
+        posicao: 'C',
+        numero: '15'
+    }
+}
